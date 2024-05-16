1020abfef6f89a622f4c3fa1cd5baa901b6f1e42
yagoluucas <yago543@gmail.com>
2024-05-16 14:15:21 -0300

    feito processo para salvar informações no local storage

--- script/alimentos-selecionados.js
+++ script/alimentos-selecionados.js
@@ -1,3 +1,14 @@
+import * as utils from './utils.js'
 window.addEventListener('DOMContentLoaded', () => {
+    const arrayAlimentos = utils.pegarInfoLocalStorage('alimentos').alimentos
+    const sectionAlimentosSalvos = document.querySelector('.js-alimentos--salvos')
+    if (arrayAlimentos.length >= 1) {
+        arrayAlimentos.forEach((item) => {
+            const alimentoConstruido = utils.construirDivAlimento(item)
+            sectionAlimentosSalvos.append(alimentoConstruido)
+        })
+    } else {
+        alert('Sem alimentos cadastrado')
+    }
     
 })
--- script/utils.js
+++ script/utils.js
@@ -1,15 +1,64 @@
-export function setarInfoInicialLocalStorage(){
+import { alimentosDaPesquisa } from './script.js'
+export function setarInfoInicialLocalStorage() {
     const alimento = localStorage.getItem('alimentos')
     if (alimento == null) {
-        localStorage.setItem('alimentos', JSON.stringify([]))
-        localStorage.setItem('chaveAlimento', 0)
+        localStorage.setItem('alimentos', JSON.stringify({ alimentos: [] }))
+        return 'informações alocadas'
     }
+
+    return 'informações já alocadas'
 }
 
-export function pegarInfoLocalStorage(info){
-    return localStorage.getItem(info)
+export function pegarInfoLocalStorage(info) {
+    return JSON.parse(localStorage.getItem(info))
 }
 
-export function setarInfoLocalStorage(info, valor){
-    localStorage.setItem(info, JSON.stringify(valor))
+export function setarInfoLocalStorage(objeto) {
+    const arrayAlimentos = pegarInfoLocalStorage('alimentos')
+    arrayAlimentos.alimentos.unshift(objeto)
+    localStorage.setItem('alimentos', JSON.stringify(arrayAlimentos))
+}
+
+
+export function construirDivAlimento(comida, id) {
+    const paragrafoFibra = document.createElement('p')
+    const paragrafoCarboidrato = document.createElement('p')
+    const paragrafoGordura = document.createElement('p')
+    const paragrafoProteina = document.createElement('p')
+    const paragrafoCalorias = document.createElement('p')
+    let btnAdicionarAlimento = null
+    if (window.location.pathname == '/index.html') {
+        btnAdicionarAlimento = document.createElement('button')
+        btnAdicionarAlimento.addEventListener('click', () => {
+            salvarInfo(btnAdicionarAlimento, alimentosDaPesquisa)
+        })
+        btnAdicionarAlimento.innerHTML = 'Adicionar Alimento'
+    }
+    console.log(btnAdicionarAlimento)
+    const divInfoAlimnto = document.createElement('div')
+    const arrayNutrientes = comida.nutrients
+    const divAlimento = document.createElement('div')
+    const imgComida = document.createElement('img')
+    const h2 = document.createElement('h2')
+    divAlimento.classList.add('card-alimento')
+    imgComida.src = comida.image == undefined ? './imagens/sem-comida.png' : comida.image
+    h2.textContent = comida.label
+    paragrafoFibra.innerHTML = `<span class="verde">Fibras </span>: ${arrayNutrientes.FIBTG.toFixed(2)}`
+    paragrafoCarboidrato.innerHTML = `<span class="verde">Carboidrato </span>: ${arrayNutrientes.CHOCDF.toFixed(2)}`
+    paragrafoGordura.innerHTML = `<span class="verde">Gordura </span>: ${arrayNutrientes.FAT.toFixed(2)}`
+    paragrafoProteina.innerHTML = `<span class="verde">Proteina </span>: ${arrayNutrientes.PROCNT.toFixed(2)}`
+    paragrafoCalorias.innerHTML = `<span class="verde">Calorias </span>: ${arrayNutrientes.ENERC_KCAL.toFixed(2)}`
+    divInfoAlimnto.append(paragrafoFibra, paragrafoCarboidrato, paragrafoGordura, paragrafoProteina, paragrafoCalorias, btnAdicionarAlimento)
+    divAlimento.append(h2, imgComida, divInfoAlimnto)
+    divAlimento.setAttribute('id', id)
+    return divAlimento
+}
+
+export function salvarInfo(e, alimentosDaPesquisa) {
+    const id = e.parentNode.parentNode.getAttribute('id')
+    alimentosDaPesquisa.forEach((item) => {
+        if (item.index == id) {
+            setarInfoLocalStorage(item.alimento)
+        }
+    })
 }
